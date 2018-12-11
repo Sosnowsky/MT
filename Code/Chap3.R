7f7f354ca29330055354917a41b50b959ee2f581
Sosnowsky <juan.manuel.losada.sosnovsky@gmail.com>
2018-12-11 14:08:50 +0100

    Nice plots in latex

--- Code/Chap3.R
+++ Code/Chap3.R
@@ -1,7 +1,12 @@
 #units: hbar=1 (Energy*Time)  t1=1(Energy),
 #electric field is measured in units of t1/ea
 
+setwd("~/Desktop/MT/Code")
 library(latex2exp)
+library(tikzDevice)
+options(tikzMetricPackages = c("\\usepackage[utf8]{inputenc}",
+                               "\\usepackage[T1]{fontenc}", "\\usetikzlibrary{calc}",
+                               "\\usepackage{amssymb}"))
 
 plot_disconnect <- function(x, y, ylim, ...) {
   Y_DISC <- 10
@@ -26,7 +31,7 @@ t2 <- 0.1 #NNN hoping integral
 delta <- 0.5 #NNN intrinsic SOI coupling
 
 U <- 10 #Interaction energy
-w <- 16 #Laser frecuency
+w <- 6 #Laser frecuency
 bessel_trunc_order <- 10
 lattice_cte <- 1
 field_energies <- seq(from=0, by=0.001, to=5)
@@ -42,35 +47,61 @@ for (n in -bessel_trunc_order:bessel_trunc_order) {
 D2_factor <- vector(mode="numeric", length=length(field_energies))
 
 for (n in -bessel_trunc_order:bessel_trunc_order) {
-  D2_factor <- D2_factor + besselJ(sqrt(3)*field_energies/sqrt(2), nu = n)^2 / (1 + n * w / U)
+  D2_factor <- D2_factor + besselJ(sqrt(2)*field_energies, nu = n)^2 / (1 + n * w / U)
 }
 
 ratioJD0 <- t1^2 / ( 2 * t2 * delta)
 ratioJD <-  ratioJD0*J1_factor/D2_factor
 
-jpeg('rplot1.jpg')
+current_file <- "NNvsNNN"
+tikz(paste0(current_file, '.tex'), width = 4, height = 4, standAlone = TRUE,
+     packages = c("\\usepackage{tikz}",
+                  "\\usepackage[active,tightpage,psfixbb]{preview}",
+                  "\\PreviewEnvironment{pgfpicture}",
+                  "\\setlength\\PreviewBorder{0pt}",
+                  "\\usepackage{amssymb}"))
 
-plot(x=field_energies, y=J1_factor, type='l', col = "green", xlab = TeX('$E$'), ylab = '', lwd = 2)
+plot(x=field_energies, y=J1_factor, type='l', col = "green", xlab = "$\\mathcal{E}$", ylab = '', lwd = 2)
 lines(x=field_energies, y = D2_factor, type = 'l', col = "red", lwd = 2)
 legend("topright", legend = c(TeX('$J_1/J_1^0$'), TeX('$D_2/D_2^0$')), fill = c("green", "red"))
 
 dev.off()
+tools::texi2pdf(paste0(current_file, '.tex'))
+system(paste0("mv ", current_file, ".pdf ../Chapters/", current_file, ".pdf"))
+#system(paste(getOption("pdfviewer"), "formula.pdf"))
 
-jpeg('rplot2.jpg')
+current_file <- "ratio"
+tikz(paste0(current_file, '.tex'), width = 4, height = 4, standAlone = TRUE,
+     packages = c("\\usepackage{tikz}",
+                  "\\usepackage[active,tightpage,psfixbb]{preview}",
+                  "\\PreviewEnvironment{pgfpicture}",
+                  "\\setlength\\PreviewBorder{0pt}",
+                  "\\usepackage{amssymb}"))
 
-plot_disconnect(x=field_energies, y = ratioJD, ylim = c(-5*ratioJD0,2*ratioJD0), col = "blue", lwd = 2, xlab = TeX('$E$'), ylab = '')
-legend("topright", legend=TeX('$J/D$'), fill = c("blue"))
+plot_disconnect(x=field_energies, y = ratioJD, ylim = c(-3*ratioJD0,2*ratioJD0), 
+                col = "blue", lwd = 2, xlab = "$\\mathcal{E}$", ylab = '$\\frac{J_1}{D_2}$')
 
 dev.off()
+tools::texi2pdf(paste0(current_file, '.tex'))
+system(paste0("mv ", current_file, ".pdf ../Chapters/", current_file, ".pdf"))
 
-EMax <- which(field_energies>1)[1]
+EMax <- which(D2_factor <= 0)[1]-1
 res_ratio <- ratioJD[1:EMax]
 res_field_energies <- field_energies[1:EMax]
 theta <- asin((res_ratio-sqrt(res_ratio^2+32))/8)
 
-jpeg('rplot3.jpg')
+current_file <- "theta"
+tikz(paste0(current_file, '.tex'), width = 4, height = 4, standAlone = TRUE,
+     packages = c("\\usepackage{tikz}",
+                  "\\usepackage[active,tightpage,psfixbb]{preview}",
+                  "\\PreviewEnvironment{pgfpicture}",
+                  "\\setlength\\PreviewBorder{0pt}",
+                  "\\usepackage{amssymb}"))
 
-plot(x = res_field_energies, y = theta, lwd = 2, col = "purple", xlab = TeX('$E$'), ylab='')
-legend("top", legend=TeX('$\\theta(E)$  '), fill = c("purple"))
+plot(x = res_field_energies, y = theta, type = 'l', lwd =2,
+     col = "purple", xlab = "$\\mathcal{E}$", ylab="$\\theta$")
 
 dev.off()
+tools::texi2pdf(paste0(current_file, '.tex'))
+system(paste0("mv ", current_file, ".pdf ../Chapters/", current_file, ".pdf"))
+
